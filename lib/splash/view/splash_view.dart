e07c90e555437651fffa20b75cb6eaeb3d365841
ahmed kadry <ahmedyoussefkadry@gmail.com>
2022-04-04 20:28:34 +0200

    add spalsh view

---
+++ lib/splash/view/splash_view.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class SplashView extends StatefulWidget {
+  const SplashView({Key? key}) : super(key: key);
+
+  @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff53B175) ,
+      body: Center(
+          child: SvgPicture.asset('assets/images/Group 1.svg')
+      ),
+    );
+  }
+}
